4087e071ce8e58c7e376d6af1c7b316df6882346
nate mellendorf <nate.mellendorf@gmail.com>
2026-02-01 11:45:56 -0800

    Update tests to use Swift 6 built-in Testing framework

--- AethosCore/Tests/AethosCoreTests/IDsTests.swift
+++ AethosCore/Tests/AethosCoreTests/IDsTests.swift
@@ -1,11 +1,11 @@
 import Foundation
-import XCTest
-
+import Testing
 @testable import AethosCore
 
-final class IDsTests: XCTestCase {
-    func testSha256Hello() {
-        let digest = AethosIDs.sha256(Data("hello".utf8))
-        XCTAssertEqual(digest.hexString, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
-    }
+@Test
+func chunkIdHello() {
+    let data = Data("hello".utf8)
+    let digest = AethosIDs.chunkId(for: data)
+    let hex = Hex.encode(digest)
+    #expect(hex == "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
 }
